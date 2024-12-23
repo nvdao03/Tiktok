b05b3448da3724d208544b85aa8455aac7226377
Nguyen Van Dao <nvdao003@gmail.com>
2024-12-23 15:47:28 +0700

    cÃonfig sass

--- .history/src/main_20241223150459.jsx
+++ .history/src/main_20241223150459.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+
 import GlobalStyles from './components/GlobalStyles/';
+import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
--- .history/src/main_20241223150512.jsx
+++ .history/src/main_20241223150512.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import GlobalStyles from './components/GlobalStyles/';
+import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
